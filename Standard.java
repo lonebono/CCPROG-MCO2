ecc6d4d60c210458d55b775545ff2fa07ccce5f3
GitHanz <hnzgabriel05@gmail.com>
2024-07-27 23:51:43 +0800

    Update Standard.java

--- Standard.java
+++ Standard.java
@@ -1,7 +1,13 @@
 package MCO2.src;
 
 public class Standard extends Room {
+	private String roomType = “STANDARD”;
+
     Standard(int roomNumber) {
         super(roomNumber);
     }
+
+	public String getRoomType() {
+		return roomType;
+	}
 }
